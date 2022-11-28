5f4341dd70eda064300c17f503693aa3fc0e68d9
Neel Aundhia <neel.aundhia@gmail.com>
2022-11-29 00:16:07 +0530

    Update get_user_profile function

--- src/tendril/authn/auth0.py
+++ src/tendril/authn/auth0.py
@@ -1,23 +1,23 @@
 import os
 import json
 
+from tendril.config import AUTH0_NAMESPACE
+os.environ['AUTH0_RULE_NAMESPACE'] = AUTH0_NAMESPACE
+
 from fastapi import Security
-from fastapi import HTTPException
 from fastapi_auth0 import Auth0, Auth0User
+
+from auth0.v3.exceptions import Auth0Error
+from auth0.v3.authentication import GetToken
+from auth0.v3.management import Auth0 as Auth0PythonClient
 
 from tendril.config import AUTH0_DOMAIN
 from tendril.config import AUTH0_AUDIENCE
-from tendril.config import AUTH0_NAMESPACE
 from tendril.config import AUTH0_USER_MANAGEMENT_API_CLIENTID
 from tendril.config import AUTH0_USER_MANAGEMENT_API_CLIENTSECRET
 
 from tendril.utils import log
 from tendril.authz import scopes
-
-from auth0.v3.authentication import GetToken
-from auth0.v3.management import Auth0 as Auth0PythonClient
-
-os.environ['AUTH0_RULE_NAMESPACE'] = AUTH0_NAMESPACE
 
 logger = log.get_logger(__name__, log.DEBUG)
 
@@ -43,8 +43,9 @@ def auth_spec(scopes=None):
         kwargs['scopes'] = scopes
     return Security(auth.get_user, **kwargs)
 
-def get_mgmt_api_token():
-    global mgmt_api_token
+
+def get_management_api_token():
+    global management_api_token
     logger.debug("Attempting to get the management API token using:")
     logger.debug("Domain: {}".format(AUTH0_DOMAIN))
     logger.debug("Client ID: {}".format(AUTH0_USER_MANAGEMENT_API_CLIENTID))
@@ -53,17 +54,28 @@ def get_mgmt_api_token():
     token = get_token.client_credentials(AUTH0_USER_MANAGEMENT_API_CLIENTID,
                                          AUTH0_USER_MANAGEMENT_API_CLIENTSECRET,
                                          'https://{}/api/v2/'.format(AUTH0_DOMAIN))
-    mgmt_api_token = token['access_token']
-    logger.debug("Successfully received Management API token ending in {}".format(mgmt_api_token[-5:]))
+    management_api_token = token['access_token']
+    logger.debug("Successfully received Management API token ending in {}".format(management_api_token[-5:]))
+
+
+def _get_user_profile(user: AuthUserModel):
+    global management_api_token
+    logger.debug("Attempting to fetch user information for {} from Auth0".format(user.id))
+    auth0 = Auth0PythonClient(AUTH0_DOMAIN, management_api_token)
+    user_profile = auth0.users.get(user.id)
+    logger.info("Got user details for {} from Auth0 Management API".format(user.id))
+    return user_profile
+
 
 def get_user_profile(user: AuthUserModel):
-    global mgmt_api_token
-
-    if mgmt_api_token is None:
-        get_mgmt_api_token()
-
-    logger.debug("Attempting to fetch user information from the Management API")
-    auth0 = Auth0PythonClient(AUTH0_DOMAIN, mgmt_api_token)
-    user_profile = auth0.users.get(user.id)
-    logger.debug("Successfully fetched user details from the Management API\n" + json.dumps(user_profile, indent=2))
-    return user_profile
+    global management_api_token
+    if management_api_token is None:
+        get_management_api_token()
+    try:
+        return _get_user_profile(user)
+    except Auth0Error as error:
+        if error.status_code == 401:
+            get_management_api_token()
+            return _get_user_profile(user)
+        else:
+            raise
