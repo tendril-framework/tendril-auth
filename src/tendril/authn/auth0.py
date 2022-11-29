9c691a4f56f0b016c180527d071f28cfe21dbc8b
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2022-11-29 20:08:35 +0530

    Implement userinfo caching.

--- src/tendril/authn/auth0.py
+++ src/tendril/authn/auth0.py
@@ -1,5 +1,6 @@
+
+
 import os
-import json
 
 from tendril.config import AUTH0_NAMESPACE
 os.environ['AUTH0_RULE_NAMESPACE'] = AUTH0_NAMESPACE
@@ -11,20 +12,31 @@ from auth0.v3.exceptions import Auth0Error
 from auth0.v3.authentication import GetToken
 from auth0.v3.management import Auth0 as Auth0PythonClient
 
+from tendril.authz import scopes
+
 from tendril.config import AUTH0_DOMAIN
 from tendril.config import AUTH0_AUDIENCE
 from tendril.config import AUTH0_USER_MANAGEMENT_API_CLIENTID
 from tendril.config import AUTH0_USER_MANAGEMENT_API_CLIENTSECRET
+from tendril.config import AUTH0_USERINFO_CACHING
 
 from tendril.utils import log
-from tendril.authz import scopes
-
 logger = log.get_logger(__name__, log.DEBUG)
 
-logger.info("Using auth0 parameters:\n"
+
+logger.info("Using Auth0 parameters:\n"
             "  - domain    {} \n"
             "  - audience  {} \n"
             "  - namespace {} ".format(AUTH0_DOMAIN, AUTH0_AUDIENCE, AUTH0_NAMESPACE))
+
+
+if AUTH0_USERINFO_CACHING == 'platform':
+    logger.info("Using platform level caching for Auth0 UserInfo")
+    from tendril.caching import platform_cache as cache
+else:
+    logger.info("Not caching Auth0 UserInfo")
+    from tendril.caching import no_cache as cache
+
 
 auth = Auth0(domain=AUTH0_DOMAIN,
              api_audience=AUTH0_AUDIENCE,
@@ -46,10 +58,13 @@ def auth_spec(scopes=None):
 
 def get_management_api_token():
     global management_api_token
-    logger.debug("Attempting to get the management API token using:")
-    logger.debug("Domain: {}".format(AUTH0_DOMAIN))
-    logger.debug("Client ID: {}".format(AUTH0_USER_MANAGEMENT_API_CLIENTID))
-    logger.debug("Client Secret ending in {}".format(AUTH0_USER_MANAGEMENT_API_CLIENTSECRET[-5:]))
+    logger.debug("Attempting to get the management API token using:\n"
+                 "  - domain          {}\n"
+                 "  - client id       {}\n"
+                 "  - client secret   ending with {}".format(
+        AUTH0_DOMAIN, AUTH0_USER_MANAGEMENT_API_CLIENTID,
+        AUTH0_USER_MANAGEMENT_API_CLIENTSECRET[-5:0])
+    )
     get_token = GetToken(AUTH0_DOMAIN)
     token = get_token.client_credentials(AUTH0_USER_MANAGEMENT_API_CLIENTID,
                                          AUTH0_USER_MANAGEMENT_API_CLIENTSECRET,
@@ -67,6 +82,11 @@ def _get_user_profile(user: AuthUserModel):
     return user_profile
 
 
+def _key_func(user: AuthUserModel):
+    return user.id
+
+
+@cache(namespace='userinfo', ttl=86400, key=_key_func)
 def get_user_profile(user: AuthUserModel):
     global management_api_token
     if management_api_token is None:
